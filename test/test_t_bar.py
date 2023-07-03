af284178990fc58a2715037a7df4f942bdc9295c
P-How <peihaoli1014@gmail.com>
2023-07-03 23:29:26 +0800

    modified test file

--- test/test_t_bar.py
+++ test/test_t_bar.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 from core.coin import Coin
 from core import utils
+from core import hft
 from binance.client import Client
 import statistics
 # Load the environment variables from the .env file
@@ -18,15 +19,5 @@ client = Client(api_key, api_secret)
 coin = Coin("BTC", client)
 d_bar_array = []
 while True:
-    prices = client.get_all_tickers()
-    p2 = float(utils.find_pair_price_from_tickers(prices,"BTCUSDT"))
-    p3 = float(utils.find_pair_price_from_tickers(prices,"BTCTUSD"))
-    p1 = float(utils.find_pair_price_from_tickers(prices,"TUSDUSDT"))
-    p3_star = float(p2/p1)
-    t_bar = (1/p1-1)*p2
-    d_bar_array.append(p3-p2)
-    if len(d_bar_array) > 100:
-        d_bar_array.pop(0)
-    d_bar = statistics.mean(d_bar_array)
-    p3_bar = p2+d_bar
-    print(f'\rBTCTUSD star price: {p3_star}, BTCTUSD bar price: {p3_bar}, dBarLength : {len(d_bar_array)}', end='', flush=True)
+    p = hft.get_p_reference(coin,d_bar_array)
+    print(f'\rBTCTUSD bar price: {p}, dBarLength : {len(d_bar_array)}', end='', flush=True)
