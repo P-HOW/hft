b17c874b2b84c4460497132edf7be65fc0a07c34
P-How <peihaoli1014@gmail.com>
2023-07-03 15:38:11 +0800

    updated tests

--- test/speedup.py
+++ test/speedup.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 from core.coin import Coin
+import time
 from core import utils
 from binance.client import Client
 import statistics
@@ -17,6 +18,25 @@ client = Client(api_key, api_secret)
 # Now you can create an instance of Coin
 coin = Coin("BTC", client)
 d_bar_array = []
+start_time_segment_2 = time.time()
 
-info = client.get_symbol_info('BTCUSDT')
-print(info)
+# ... (your second segment of code here) ...
+prices = client.get_all_tickers()
+# Measure the end time of the second segment
+end_time_segment_2 = time.time()
+
+# Calculate the elapsed time for the second segment
+elapsed_time_segment_2 = end_time_segment_2 - start_time_segment_2
+
+print(f"Time elapsed in segment 2: {elapsed_time_segment_2} seconds")
+start_time_segment_1 = time.time()
+#info = client.get_symbol_info('BTCUSDT')
+#depth1 = client.get_order_book(symbol='BTCUSDT')
+#depth2 = client.get_order_book(symbol='BTCTUSD')
+avg_price = client.get_avg_price(symbol='BTCUSDT')
+avg_price = client.get_avg_price(symbol='BTCTUSD')
+end_time_segment_1 = time.time()
+elapsed_time_segment_1 = end_time_segment_1 - start_time_segment_1
+
+print(f"Time elapsed in segment 1: {elapsed_time_segment_1} seconds")
+
--- test/trade_history_test.py
+++ test/trade_history_test.py
@@ -18,12 +18,12 @@ client = Client(api_key, api_secret)
 # Now you can create an instance of Coin
 coin = Coin("BTC", client)
 
-my_trades = coin.get_trades_history("TUSD")
+#my_trades = coin.get_trades_history("TUSD")
 #my_trades = []
-print(utils.summarize_24h_volume(my_trades))
+#print(utils.summarize_24h_volume(my_trades))
 # Get the current time (in milliseconds)
-
-
+avg_price = client.get_avg_price(symbol='BTCUSDT')
+print(avg_price)
 #print(f"Your 24-hour trading volume for BTCTUSD: {volume} TUSD")
 #status = client.get_account_api_trading_status()
 #print (status)
