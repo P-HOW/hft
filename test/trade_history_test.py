2d207866610c44e83802707233f7122f8b94001f
P-How <peihaoli1014@gmail.com>
2023-07-05 21:03:02 +0800

    modified test files

--- test/trade_history_test.py
+++ test/trade_history_test.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
-from core.coin import Coin
+
+from core import coin
 import time
 from binance.client import Client
 
@@ -16,11 +17,14 @@ api_secret = os.getenv("API_SECRET")
 client = Client(api_key, api_secret)
 
 # Now you can create an instance of Coin
-coin = Coin("BTC", client)
-
+coin = coin.Coin("BTC", client)
+coin.add_pair("TUSD")
 my_trades = coin.get_my_trades("TUSD")
 #my_trades = []
-print(utils.summarize_24h_volume(my_trades))
+#print(utils.summarize_24h_volume(my_trades))
+#coin.guaranteed_cancel_orders_above_threshold("TUSD", 0)
+#print(coin.get_last_buy_average_price("TUSD"))
+coin.guaranteed_cancel_all_open_orders("TUSD")
 # Get the current time (in milliseconds)
 #avg_price = client.get_avg_price(symbol='BTCUSDT')
 #print(avg_price)
