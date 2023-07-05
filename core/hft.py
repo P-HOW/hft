cce00fd4e53e8c10d9c684d7cbb9a92e2ced8604
P-How <peihaoli1014@gmail.com>
2023-07-05 21:02:42 +0800

    added stop losses module

--- core/hft.py
+++ core/hft.py
@@ -45,7 +45,6 @@ def wait_for_pending_BTC_orders(min_qty, coin, d_bar_array, k1, k2, d_bar_array_
     _, l = coin.guaranteed_get_balance("BTC")
     while l > min_qty:
         if time.time() - start_time > TIME_OUT:
-            print("Time limit exceeded, exiting function")
             return True
         get_p_reference(coin, d_bar_array, k1, k2, d_bar_array_length)
         _, l = coin.guaranteed_get_balance("BTC")
@@ -53,24 +52,33 @@ def wait_for_pending_BTC_orders(min_qty, coin, d_bar_array, k1, k2, d_bar_array_
 
 
 def place_stop_loss_orders(coin, pair, n_orders, last_buy_price, current_price, d_bar_array, k1, k2, d_bar_array_length,
-                           minBTC, fault_tolerance, step_size, TIME_OUT):
+                           minBTC, fault_tolerance, step_size, TIME_OUT, order_bias, total_value):
     start_time = time.time()  # Get the current time
     ul, _ = coin.guaranteed_get_balance("BTC")
+    print("remaining bitcions: " + str(ul))
+    cleared_percentage = ul*current_price / total_value
+    n_orders = max(math.floor(n_orders * cleared_percentage), 1)
     price_decrement = (last_buy_price - current_price) / n_orders
     if ul < minBTC:
+        print("no bitcoin anymore, exit....")
         return True
     if current_price >= last_buy_price or price_decrement <= step_size:
+        print("conditions not met, exit....")
         count = 0
-        while coin.sell_at_market_price is None and count < fault_tolerance:
+        coin.guaranteed_cancel_all_open_orders("TUSD")
+        ul, _ = coin.guaranteed_get_balance("BTC")
+        while coin.sell_at_market_price("USDT", ul * order_bias) is None and count < fault_tolerance:
             count = count + 1
             get_p_reference(coin, d_bar_array, k1, k2, d_bar_array_length)
+            ul, _ = coin.guaranteed_get_balance("BTC")
         return True
     # Calculate the price increment for each order
 
     # Calculate the quantity for each order. Here, I'm just using a placeholder value.
     # You should replace this with the actual logic for calculating the order quantity.
-    quantity = ul
-    n_orders = min(n_orders, math.floor(ul/minBTC)-1)
+
+    # n_orders = min(n_orders, math.floor(ul / minBTC) - 1)
+    quantity = ul / n_orders
     # Place the orders
     for i in range(n_orders):
         limit_price = last_buy_price - (i * price_decrement)
@@ -83,21 +91,35 @@ def place_stop_loss_orders(coin, pair, n_orders, last_buy_price, current_price, 
     return True
 
 
-def stop_loss_thread(coin, pair, n_orders, last_buy_price, current_price, n_rounds, stop_loss_time, d_bar_array, k1, k2,
-                     d_bar_array_length, minBTC):
+def stop_loss_thread(coin, pair, n_orders, n_rounds, stop_loss_time, d_bar_array, k1, k2,
+                     d_bar_array_length, minBTC, order_bias):
+    print("enter stop_loss_thread")
+    _, current_price = get_p_reference(coin, d_bar_array, k1, k2, d_bar_array_length)
+    ul, l = coin.guaranteed_get_balance("BTC")
+    total_value = current_price * (ul + l)
+    last_buy_price = coin.get_last_buy_average_price("TUSD")
+    # last_buy_price = 31100
+
+    if last_buy_price < 100 or last_buy_price is None:
+        last_buy_price = current_price + 100
     step_size = coin.get_step_size("TUSD")
     fault_tolerance = 10
     sleep_time = stop_loss_time / n_rounds
     for _ in range(n_rounds):
-        coin.guaranteed_cancel_orders_above_threshold(pair, 0)
-
+        coin.guaranteed_cancel_all_open_orders("TUSD")
+        _, current_price = get_p_reference(coin, d_bar_array, k1, k2, d_bar_array_length)
+        print("stop loss mini loop entered...")
         if place_stop_loss_orders(coin, pair, n_orders, last_buy_price, current_price, d_bar_array, k1, k2,
-                               d_bar_array_length,
-                               minBTC, fault_tolerance, step_size, sleep_time):
+                                  d_bar_array_length,
+                                  minBTC, fault_tolerance, step_size, sleep_time, order_bias, total_value):
+            print("stop_loss succeed, exit...")
             return
-
+    print("time is up, exit stop loss by selling all BTC...")
+    coin.guaranteed_cancel_all_open_orders("TUSD")
     count = 0
-    while coin.sell_at_market_price is None and count < fault_tolerance:
+    ul, _ = coin.guaranteed_get_balance("BTC")
+    while coin.sell_at_market_price("USDT", ul * order_bias) is None and count < fault_tolerance:
         count = count + 1
         get_p_reference(coin, d_bar_array, k1, k2, d_bar_array_length)
-
+        ul, _ = coin.guaranteed_get_balance("BTC")
+    print("exit stop_loss_thread")
